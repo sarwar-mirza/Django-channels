993bef10bb612aa74818850a13c8e88836ac161d
mithu <nilchirkutpoison@gmail.com>
2023-12-07 14:56:59 +0600

    Setup asgi url

---
+++ asgi_url_1/asgi_url_1/asgi.py
@@ -0,0 +1,20 @@
+"""
+ASGI config for asgi_url_1 project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+from channels.routing import ProtocolTypeRouter           # import routing
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'asgi_url_1.settings')
+
+application = ProtocolTypeRouter({                  # custom application
+    'http': get_asgi_application(),
+})
